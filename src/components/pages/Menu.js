341d74d3dc76c434db1d70515e75da2ea47de2c9
predotakinga <predotakinga@gmail.com>
2021-11-21 15:58:10 +0100

    :sparkles:mobile devices pt.2

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -17,23 +17,21 @@ export default function Menu() {
       <div className="text-items">
       <h1>Menu</h1>
       <h2>Na pytanie naszych gości o to, co polecamy - naszą pierwszą odpowiedzią jest, że wszystko. Menu jest tak małe, że jest w nim tylko to co uważamy za najlepsze. Zawsze chętnie opowiemy o tym co można u nas znaleźć, jak to zjeść i co do tego dodać. Jeżeli czegoś nie ma w menu, zachęcamy do zadawania pytań, jesteśmy otwarci na zmiany i bywa, że nawet z tacosa można zrobić burrito.  </h2>
-      <div className="width-buttom">
         <Button
-            className="btns"
+            className="btn"
             buttonStyle="btn--outline"
             buttonSize="btn--large"
             onClick={openMenu}
           >ZOBACZ MENU
         </Button>
         <Button
-            className="btns"
+            className="btn"
             buttonStyle="btn--outline"
             buttonSize="btn--large"
             onClick={openOrders}
-          >ZAMÓW
+          >ZAMÓW ONLINE
         </Button>
       </div>  
-      </div>
     </div>
   ) 
 }
